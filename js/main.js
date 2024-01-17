f54b6fefc2cc4e7497f77e6ef17c7cb1e1e2d220
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2024-01-17 14:05:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1038,7 +1038,7 @@ require([
             }
 
             const edits = {
-                addFeatures: airway
+                addFeatures: [airway]
             }
 
             uamLyr.applyEdits(edits).then((results) => {
